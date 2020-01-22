ad2e8f470ca108ec829a9ed21dcbff5c4bfbdcb0
mishushakov <mishushakov@users.noreply.github.com>
2020-01-22 12:22:10 +0000

    Deploy

---
+++ precache-manifest.a82019ae17eb892f6d60eec67c78bc48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abad9fb97b297d8006aa",
+    "url": "css/app.93563c54.css"
+  },
+  {
+    "revision": "2b9cb912b95c35649da76ac7e3f9ce25",
+    "url": "img/opengraph.png"
+  },
+  {
+    "revision": "ae52b6b4d8525c1cb729685aed104f79",
+    "url": "index.html"
+  },
+  {
+    "revision": "abad9fb97b297d8006aa",
+    "url": "js/app.1cd22063.js"
+  },
+  {
+    "revision": "ce3d9d52003c4a6acafc",
+    "url": "js/chunk-vendors.a0d8f508.js"
+  },
+  {
+    "revision": "bfae9faebbd4ca00d77737a88e057227",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "robots.txt"
+  }
+]);
